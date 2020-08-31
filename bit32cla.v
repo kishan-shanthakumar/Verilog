50eb579a3a06da8dc605538eddecc5a005e20748
KISHAN S <69047931+kishan-shanthakumar@users.noreply.github.com>
2020-08-31 16:42:08 +0530

    Update bit32cla.v

--- bit32cla.v
+++ bit32cla.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 24.08.2020 12:28:18
-// Design Name: 
-// Module Name: bit32cla
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module bit32cla(a,b,cin,sum,cout);
 input[31:0] a,b;
 input cin;
