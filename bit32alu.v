beb828d951134fa74c2627bc1a9ea90e21a8e354
KISHAN S <69047931+kishan-shanthakumar@users.noreply.github.com>
2020-08-31 16:42:42 +0530

    Update bit32alu.v

--- bit32alu.v
+++ bit32alu.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 21.08.2020 17:04:44
-// Design Name: 
-// Module Name: bit32alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 module fa(
 input a,b,c,
 output s,cout
